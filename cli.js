ec1b198ee04371e46b7ab39405e2f23d00bcf9f9
Laurence Elsdon <git@elsdon.me>
2015-07-14 16:32:33 +0100

    Refactored terminal.js

--- cli.js
+++ cli.js
@@ -6,11 +6,8 @@ var terminal = require('./lib/terminal')
 // Get command line args
 var arg = process.argv.slice(2)
 
-// Without this, we would only get streams once enter is pressed
-process.stdin.setRawMode(true)
-// Resume stdin in the parent process (node app won't quit all by itself
-// unless an error or process.exit() happens)
-process.stdin.resume()
+// Capture user input
+terminal.init()
 
 // Backup
 if(arg[0] == 'backup') {
@@ -22,7 +19,7 @@ else if(arg[0] == 'restore') {
 }
 // Invalid
 else {
-  terminal('Choose an operation', ['Backup', 'Restore'], function(error, selection, index) {
+  terminal.write('Choose an operation', ['Backup', 'Restore'], function(error, selection, index) {
     if(selection == 'Backup') {
       backup()
     }
@@ -30,7 +27,7 @@ else {
       restore()
     }
     else {
-      process.stdin.end()
+      terminal.end()
     }
   })
 }
